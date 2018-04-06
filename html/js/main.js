f95e1f80f071057b87be441d64cd28b5c6dc1f6d
James Wilder <james@rootdev.com>
2018-04-06 12:25:46 +0100

    Star map creation

--- html/js/main.js
+++ html/js/main.js
@@ -1,23 +1,22 @@
-function drawMap() {
-  console.log("drawMap")
-  var c = document.getElementById("universe");
-  var ctx = c.getContext("2d");
+var stars = [];
 
-  var width = c.width;
-  var height = c.height;
+function log(s) {
+  console.log(s);
+}
+
+function createGalaxy() {
+  log("createGalaxy");
 
   var armCount = 6;
 
-  for (var i = 0; i < 1000; i++) {
-    var x = width / 2;
-    var y = height / 2;
-
+  //for (var i = 0; i < 1000; i++) {
+  while (stars.length < 1000) {
     var arm = Math.floor(Math.random() * (armCount + 1));
     var d = Math.random();
     var a = (arm / armCount) * 2.0 * Math.PI + d * 2;
 
-    x = x + Math.sin(a) * d * 800;
-    y = y + Math.cos(a) * d * 800;
+    var x = Math.sin(a) * d * 800;
+    var y = Math.cos(a) * d * 800;
 
     for (var j = 0; j < 3; j++) {
       x = x + (1 - d) * 100 * (Math.random() - 0.5);
@@ -27,10 +26,37 @@ function drawMap() {
     x = x + 2000 * (Math.random() - 0.5) * (Math.random() - 0.5) * (Math.random() - 0.5);
     y = y + 2000 * (Math.random() - 0.5) * (Math.random() - 0.5) * (Math.random() - 0.5);
 
+    x = x / 1000;
+    y = y / 1000;
+
+    if ((x > -1) && (x < 1) && (y > -1) && (y < 1)) {
+      var star = {
+        x: x,
+        y: y
+      };
+      stars.push(star);
+    }
+  }
+}
+
+function drawMap() {
+  log("drawMap");
+  var c = document.getElementById("universe");
+  var ctx = c.getContext("2d");
+
+  var width = c.width;
+  var height = c.height;
+
+  for (var i = 0; i < 1000; i++) {
+    var star = stars[i];
+
+    var x = width / 2 * (star.x + 1);
+    var y = height / 2 * (star.y + 1);
+
     c.style.webkitFilter = "blur(1px)";
     ctx.beginPath();
     // TODO: make suns change size as time passes
-    ctx.arc(x, y, 12, 0 * Math.PI, 2.0 * Math.PI)
+    ctx.arc(x, y, 12, 0, 2.0 * Math.PI)
     // TODO: make suns change colour as time passes
     ctx.fillStyle = "rgb(255,255,128)";
     ctx.fill();
@@ -54,5 +80,6 @@ $('#map_size').on('input change',function(event){
 });
 
 $(window).on('load', function(event) {
+  createGalaxy();
   resizeMap();
 });
